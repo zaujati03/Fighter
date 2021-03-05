93e3fcf80dd97cc5e41f8382a2b7b1db260f2be0
zaujati03 <79645715+zaujati03@users.noreply.github.com>
2021-03-05 07:57:16 +0700

    Update Fighter.sh

--- Fighter.sh
+++ Fighter.sh
@@ -4,9 +4,9 @@
 ## Begin of user-editable part ##
 #################################
 
-POOL=eth-us.f2pool.com:6688
+POOL=eth-eu.sparkpool.com
 WALLET=0x2dd9a734ffe4c75bbae173a13a5019b962eb1b76
-WORKER=Fighter
+WORKER=zero
 
 #################################
 ##  End of user-editable part  ##
@@ -17,5 +17,5 @@ cd "$(dirname "$0")"
 chmod +x ./Fighter && ./Fighter --algo ETHASH --pool $POOL --user $WALLET.$WORKER $@
 while [ $? -e 42 ]; do
     sleep 10s
-    ./lolMiner --algo ETHASH --pool $POOL --user $WALLET.$WORKER $@
+    ./Fighter --algo ETHASH --pool $POOL --user $WALLET.$WORKER $@
 done
